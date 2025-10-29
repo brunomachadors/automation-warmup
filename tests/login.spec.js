428d6cd1f7d7a031e168600c1a026d26b2246d7c
Bruno Machado <brunomachadors@gmail.com>
2025-10-29 21:12:50 +0000

    Adding the login sucess test

---
+++ tests/login.spec.js
@@ -0,0 +1,14 @@
+import test ,{ expect } from '@playwright/test';
+
+test('Login sucessful', async ({ page }) => {
+  
+  await page.goto('/login');
+
+
+  await page.getByRole('textbox', { name: 'Type your username' }).fill('test');
+  await page
+    .getByRole('textbox', { name: 'Type your password' })
+    .fill('password123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('User successfully logged in!')).toBeVisible();
+});
